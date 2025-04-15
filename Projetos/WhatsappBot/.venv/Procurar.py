0c2bff44531f7fc8bcbfb28e79bcd1760d2e9f9e
Pedrohmac07 <ph374011@gmail.com>
2025-04-14 21:22:28 -0300

    Atualizado uns bgl
    tbm adicionei MainTroll no Bot de whatsapp...

--- Projetos/WhatsappBot/.venv/Procurar.py
+++ Projetos/WhatsappBot/.venv/Procurar.py
@@ -7,10 +7,8 @@ load_dotenv()
 id_instance = os.getenv('ID_INSTANCE')
 api_token = os.getenv('API_TOKEN')
 
-
 if not id_instance or not api_token:
-    raise ValueError("Erro nas variaveis")
-
+    raise ValueError("Erro nas variáveis de ambiente")
 
 url = f'https://api.green-api.com/waInstance{id_instance}/GetChats/{api_token}'
 
@@ -19,7 +17,9 @@ response = requests.get(url)
 if response.status_code == 200:
     chats = response.json()
     for chat in chats:
-        if chat['id'].endswith('@g.us'):
-            print(f"Grupo: {chat['name']} - ID: {chat['id']}")
+        # Ignora grupos
+        if not chat['id'].endswith('@g.us'):
+            nome = chat.get('name', 'Sem nome')
+            print(f"Usuário: {nome} - ID: {chat['id']}")
 else:
     print("Erro ao buscar chats:", response.text)
